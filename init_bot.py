d248a1e5fc950ef7f807a8ff4b04bc46e4c9dfb8
Vez3R <144614161+Vez3R@users.noreply.github.com>
2024-06-14 23:24:44 +0300

    init_bot.py

--- init_bot.py
+++ init_bot.py
@@ -5,7 +5,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from init_db import DB
 
 storage = MemoryStorage()
-API_TOKEN = '' #Введите токен бота
+API_TOKEN = '7075127081:AAFLuEc4Yzu9SsQj0446hA-m85udcf5rlxQ' #Введите токен бота
 
 scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
 db = DB("users.db")
